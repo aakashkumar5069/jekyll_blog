220bd13be2daa498a796e70a00783212e71b0987
aakashkumar5069 <aakashkumar5069@gmail.com>
2019-06-07 23:07:18 +0530

    try2

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -48,16 +48,6 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
             })
         },
         filter: function() {
